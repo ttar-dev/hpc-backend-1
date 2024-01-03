12ff6178e9ec3e77cf0d2586703b51df245180d3
Nattawut Kitiwan <ta.nattawut@icloud.com>
2024-01-03 19:02:17 +0700

    Add routes and response middleware

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
 const constants = require("./constants");
+const routes = require("./routes");
+const { handleSuccess, handleError } = require("./middleware/response");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use((req, res, next) => {
+  req.responseError = handleError;
+  req.responseSuccess = handleSuccess;
+  next();
+});
+
+app.use("/", routes);
 
 app.listen(constants.port, async function () {
   console.log("Service Started", constants.port);
