2dc8a4c0a900681999e674e5fb117f57743973d9
Nattawut Kitiwan <ta.nattawut@icloud.com>
2024-01-03 19:06:54 +0700

    Update routes for todo.public.js

--- routes/todo.public.js
+++ routes/todo.public.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/").get(async (req, res) => {
+router.route("/lists").get(async (req, res) => {
+  try {
+    req.responseSuccess(res, {});
+  } catch (error) {
+    req.responseError(res, error);
+  }
+});
+
+router.route("/list/:id").get(async (req, res) => {
+  try {
+    req.responseSuccess(res, {});
+  } catch (error) {
+    req.responseError(res, error);
+  }
+});
+
+router.route("/list/:id").put(async (req, res) => {
+  try {
+    req.responseSuccess(res, {});
+  } catch (error) {
+    req.responseError(res, error);
+  }
+});
+
+router.route("/list/:id").delete(async (req, res) => {
+  try {
+    req.responseSuccess(res, {});
+  } catch (error) {
+    req.responseError(res, error);
+  }
+});
+
+router.route("/list").post(async (req, res) => {
   try {
     req.responseSuccess(res, {});
   } catch (error) {
