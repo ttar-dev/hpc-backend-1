93903a24b6ecc1e5beca484289fc2aa12b73d448
Nattawut Kitiwan <ta.nattawut@icloud.com>
2024-01-03 19:22:46 +0700

    Add validation and error handling to GET, PUT, DELETE, and POST routes

--- routes/todo.public.js
+++ routes/todo.public.js
@@ -1,44 +1,92 @@
 const express = require("express");
 const router = express.Router();
+const { validationResult, body, param } = require("express-validator");
+let lists = [];
 
-router.route("/lists").get(async (req, res) => {
+router.route("/").get(async (req, res) => {
   try {
-    req.responseSuccess(res, {});
+    req.responseSuccess(res, { data: lists });
   } catch (error) {
     req.responseError(res, error);
   }
 });
 
-router.route("/list/:id").get(async (req, res) => {
-  try {
-    req.responseSuccess(res, {});
-  } catch (error) {
-    req.responseError(res, error);
-  }
-});
+router
+  .route("/:id")
+  .get(
+    [param("id").notEmpty().withMessage("ID is required")],
+    async (req, res) => {
+      try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          throw { statusCode: 400, message: errors.array() };
+        }
+        req.responseSuccess(res, {});
+      } catch (error) {
+        req.responseError(res, error);
+      }
+    }
+  );
 
-router.route("/list/:id").put(async (req, res) => {
-  try {
-    req.responseSuccess(res, {});
-  } catch (error) {
-    req.responseError(res, error);
-  }
-});
+router
+  .route("/:id")
+  .put(
+    [param("id").notEmpty().withMessage("ID is required")],
+    async (req, res) => {
+      try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          throw { statusCode: 400, message: errors.array() };
+        }
+        req.responseSuccess(res, {});
+      } catch (error) {
+        req.responseError(res, error);
+      }
+    }
+  );
 
-router.route("/list/:id").delete(async (req, res) => {
-  try {
-    req.responseSuccess(res, {});
-  } catch (error) {
-    req.responseError(res, error);
-  }
-});
+router
+  .route("/:id")
+  .delete(
+    [param("id").notEmpty().withMessage("ID is required")],
+    async (req, res) => {
+      try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          throw { statusCode: 400, message: errors.array() };
+        }
+        req.responseSuccess(res, {});
+      } catch (error) {
+        req.responseError(res, error);
+      }
+    }
+  );
 
-router.route("/list").post(async (req, res) => {
-  try {
-    req.responseSuccess(res, {});
-  } catch (error) {
-    req.responseError(res, error);
-  }
-});
+router
+  .route("/")
+  .post(
+    [body("desc").notEmpty().withMessage("Description is required")],
+    async (req, res) => {
+      try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          throw { statusCode: 400, message: errors.array() };
+        }
+
+        const { desc } = req.body;
+
+        const newList = { id: lists?.length + 1, desc };
+        lists.push(newList);
+
+        req.responseSuccess(res, {
+          statusCode: 201,
+          message: "created.",
+          data: lists,
+        });
+      } catch (error) {
+        req.responseError(res, error);
+      }
+    }
+  );
 
 module.exports = router;
